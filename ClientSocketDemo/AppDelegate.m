c72aea5352e56b5644498f4a3062fe2d540fc1bd
baochan <baochan.guo@yinyuetai.com>
2017-07-31 11:39:14 +0800

    sourcetree_test1

--- ClientSocketDemo/AppDelegate.m
+++ ClientSocketDemo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"sourcetree的test");
+    NSLog(@"sourcetree的test1");
     return YES;
 }
 
